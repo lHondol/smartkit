f9544c83fc1036b07be2ec8b4a2e950c374e5cba
xtream-00 <joseearmandoo26@gmail.com>
2022-04-15 14:34:54 +0700

    fixed get bundle issue

--- app/Http/Controllers/BundleController.php
+++ app/Http/Controllers/BundleController.php
@@ -140,7 +140,8 @@ class BundleController extends Controller
 
     public function get(Request $req)
     {
-        $bundle = Bundle::find($req->id, $this->toSelect)
+        $bundle = Bundle::find($req->id)
+            ->select($this->toSelect)
             ->with('products')
             ->first();
         return response()->json([
